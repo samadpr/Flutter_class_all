fb2a4dea0238a2e2a438d2739b376c50c6c3ce79
Abdul Samad <codinguse341@gmail.com>
2023-01-24 17:37:18 +0530

    SQFlite finished but its not working some any other problems

--- HIVE & SQFlite/hive_sample_part_2/lib/db/functions/db_functions.dart
+++ HIVE & SQFlite/hive_sample_part_2/lib/db/functions/db_functions.dart
@@ -1,23 +1,46 @@
-import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/material.dart';
 import 'package:hive_sample/db/model/data_model.dart';
+import 'package:sqflite/sqflite.dart';
 
 ValueNotifier<List<StudentModel>> studentListNotifier = ValueNotifier([]);
+
+late Database _db;
+
+Future<void> initializeDataBase() async {
+  _db = await openDatabase(
+    'student.db',
+    version: 1,
+    onCreate: (Database db, int version) async {
+      await db.execute(
+          'CREATE TABLE student (id INTEGER PRIMARY KEY, name TEXT, age TEXT)');
+    },
+  );
+}
 
 Future<void> addStudent(StudentModel value) async {
   // final studentDB = await Hive.openBox<StudentModel>('student_db');
   // final _id = await studentDB.add(value);
   // value.id = _id;
-  studentListNotifier.value.add(value);
-  studentListNotifier.notifyListeners();
+  await _db.rawInsert(
+      'INSERT INTO student (name,age) VALUES (?,?)', [value.name, value.age]);
+  getAllStudents();
+  // studentListNotifier.value.add(value);
+  // studentListNotifier.notifyListeners();
 }
 
 Future<void> getAllStudents() async {
   // final studentDB = await Hive.openBox<StudentModel>('student_db');
+
+  final _values = await _db.rawQuery('SELECT * FROM student');
+  print(_values);
   studentListNotifier.value.clear();
+  _values.forEach((map) {
+    final student = StudentModel.fromMap(map);
+    studentListNotifier.value.add(student);
+    studentListNotifier.notifyListeners();
+  });
 
   // studentListNotifier.value.addAll(studentDB.values);
-  studentListNotifier.notifyListeners();
 }
 
 Future<void> deletestudent(int id) async {
--- HIVE & SQFlite/hive_sample_part_2/lib/main.dart
+++ HIVE & SQFlite/hive_sample_part_2/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_sample/db/functions/db_functions.dart';
 
-import 'package:hive_sample/db/model/data_model.dart';
 import 'package:hive_sample/screens/home/screen_home.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await initializeDataBase();
   runApp(const MyApp());
 }
 
@@ -18,18 +18,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ScreenHome(),
+      home: const ScreenHome(),
     );
   }
 }
